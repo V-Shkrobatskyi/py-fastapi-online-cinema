572697fc4c08e790d5a1628bbf66c9700dd20192
Vitalii <vetali5700gmail.com>
2025-05-15 21:29:51 +0300

    add movies.py and carts.py with models to database

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -10,5 +10,23 @@ from .models.accounts import (
     PasswordResetToken,
     RefreshToken,
 )
+from .models.movies import (
+    MoviesGenres,
+    MoviesDirectors,
+    MoviesStars,
+    Genre,
+    Star,
+    Director,
+    Certification,
+    Movie,
+    Comment,
+    AnswerComment,
+    Favorite,
+    Like,
+    Dislike,
+    Rating,
+)
+from .models.carts import Cart, CartItem, Purchased
 from .database import get_db, get_db_contextmanager, reset_database
+
 from .validators import accounts as accounts_validators
