d87618af66a1b046da93e0e4923936e57bf04d8d
Vitalii <vetali5700gmail.com>
2025-05-24 23:01:10 +0300

    add PASSWORD_CHANGE_NAME variable

--- src/config/settings.py
+++ src/config/settings.py
@@ -15,6 +15,7 @@ class BaseAppSettings(BaseSettings):
     ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME: str = "activation_complete.html"
     PASSWORD_RESET_TEMPLATE_NAME: str = "password_reset_request.html"
     PASSWORD_RESET_COMPLETE_TEMPLATE_NAME: str = "password_reset_complete.html"
+    PASSWORD_CHANGE_NAME: str = "password_change.html"
 
     LOGIN_TIME_DAYS: int = 7
 
