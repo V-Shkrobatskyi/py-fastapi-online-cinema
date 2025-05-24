76a5a205c19290975f556ace4d217b67af8ace96
Vitalii <vetali5700gmail.com>
2025-05-24 22:59:22 +0300

    add routers for profiles, movies and carts

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
+import uvicorn
 from fastapi import FastAPI
 
-from routes import accounts_router
+from routes import accounts_router, profiles_router, movies_router, carts_router
 
 app = FastAPI(
     title="Online cinema",
     description="Online Cinema project based on FastAPI and SQLAlchemy",
 )
 
-app.include_router(accounts_router, prefix=f"/accounts", tags=["accounts"])
+app.include_router(accounts_router, prefix="/accounts", tags=["accounts"])
+app.include_router(profiles_router, prefix="/profiles", tags=["profiles"])
+app.include_router(movies_router, prefix="/movies", tags=["movies"])
+app.include_router(carts_router, prefix="/carts", tags=["carts"])
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
