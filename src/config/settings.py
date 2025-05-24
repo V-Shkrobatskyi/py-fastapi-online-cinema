37138e27e2b54abae76119539b3f2c2ecb1954ab
Vitalii <vetali5700gmail.com>
2025-05-24 23:01:59 +0300

    add POSTGRES variables

--- src/config/settings.py
+++ src/config/settings.py
@@ -45,6 +45,12 @@ class BaseAppSettings(BaseSettings):
 
 
 class Settings(BaseAppSettings):
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER", "test_user")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "test_password")
+    POSTGRES_HOST: str = os.getenv("POSTGRES_HOST", "test_host")
+    POSTGRES_DB_PORT: int = int(os.getenv("POSTGRES_DB_PORT", 5432))
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "test_db")
+
     SECRET_KEY_ACCESS: str = os.getenv("SECRET_KEY_ACCESS", os.urandom(32))
     SECRET_KEY_REFRESH: str = os.getenv("SECRET_KEY_REFRESH", os.urandom(32))
     JWT_SIGNING_ALGORITHM: str = os.getenv("JWT_SIGNING_ALGORITHM", "HS256")
